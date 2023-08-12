b8fefa9f137c2e3a7b66d5bfddfee6d8933dd5e9
Alex-Dono <alessandro.casabona.94@gmail.com>
2023-08-12 19:23:20 +0200

    added requesting and setting location if permission granted

--- app/src/main/java/com/timetoeat/MainActivity.kt
+++ app/src/main/java/com/timetoeat/MainActivity.kt
@@ -1,13 +1,88 @@
 package com.timetoeat
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.timetoeat.databinding.ActivityMainBinding
 
+private lateinit var locationManager: LocationManager
+private var userLocation: Location? = null
+
+private val locationListener: LocationListener = object : LocationListener {
+    override fun onLocationChanged(location: Location) {
+        userLocation = location
+        Log.i("Location", "Latitude: ${location.latitude}, Longitude: ${location.longitude}")
+        // Stop receiving updates after the first one
+        locationManager.removeUpdates(this)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+
+    override fun onProviderEnabled(provider: String) {}
+
+    override fun onProviderDisabled(provider: String) {}
+}
+
 private lateinit var binding: ActivityMainBinding
+private const val MY_PERMISSIONS_REQUEST_LOCATION = 1
 
 class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            // Request permission
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                MY_PERMISSIONS_REQUEST_LOCATION)
+        } else {
+            // If permission is already granted, get the location
+            requestUserLocation()
+        }
+    }
 
+    private fun requestUserLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                0L,
+                0f,
+                locationListener
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_LOCATION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission was granted
+                    Log.i("Message:", "Allowed")
+                    requestUserLocation()
+                } else {
+                    // Permission was denied
+                    Log.i("Message:", "Denied")
+                }
+                return
+            }
+            // Handle other permission requests here if needed
+        }
+    }
 
 }
--- app/src/main/java/com/timetoeat/RegistrationActivity.kt
+++ app/src/main/java/com/timetoeat/RegistrationActivity.kt
@@ -1,19 +1,15 @@
 package com.timetoeat
 
-import android.Manifest
-import android.content.pm.PackageManager
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.timetoeat.databinding.ActivityRegistrationBinding
 
 private lateinit var binding: ActivityRegistrationBinding
-private const val MY_PERMISSIONS_REQUEST_LOCATION = 1
 
 class RegistrationActivity : AppCompatActivity() {
 
@@ -33,14 +29,6 @@ class RegistrationActivity : AppCompatActivity() {
 
         binding.signIn.setOnClickListener {
             signIn(binding.email.text.toString(), binding.password.text.toString())
-        }
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-            != PackageManager.PERMISSION_GRANTED) {
-            // Request permission
-            ActivityCompat.requestPermissions(this,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                MY_PERMISSIONS_REQUEST_LOCATION)
         }
     }
 
@@ -66,7 +54,6 @@ class RegistrationActivity : AppCompatActivity() {
                         Toast.makeText(baseContext, "Authentication failed.",
                             Toast.LENGTH_SHORT).show()
                     }
-                    updateUI(null)
                 }
             }
     }
@@ -107,7 +94,6 @@ class RegistrationActivity : AppCompatActivity() {
                         Toast.makeText(baseContext, "Registration failed.",
                             Toast.LENGTH_SHORT).show()
                     }
-                    updateUI(null)
                 }
             }
     }
@@ -132,24 +118,7 @@ class RegistrationActivity : AppCompatActivity() {
     }
 
     private fun updateUI(user: FirebaseUser?) {
-        // Update your UI here based on user authentication status
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            MY_PERMISSIONS_REQUEST_LOCATION -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // Permission was granted
-                    Log.i("Message:", "Allowed")
-                } else {
-                    // Permission was denied
-                    Log.i("Message:", "Denied")
-                }
-                return
-            }
-            // Handle other permission requests here if needed
-        }
+        startActivity(Intent(this, MainActivity::class.java))
     }
 
 }
